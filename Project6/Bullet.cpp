d4744a8c3f9e76ec433b458e618b5c0e8d92afad
dismooont <savtchenko.dima@gmail.com>
2024-08-29 21:30:42 +0300

    Added Curves

--- Project6/Bullet.cpp
+++ Project6/Bullet.cpp
@@ -4,13 +4,22 @@
 Bullet::Bullet(Texture2D texture, float xAimPoint, float yAimPoint)
 {
 	bulletTexture = texture;
-	for (int i = 100; i > 0; i--) {
+	for (float i = 0; i <= 100; i++) {
+		float i2 = i/100 * i/100;
+		float i3 = i2 * i/100;
+
+		float b0 = 1 - 3 * i/100 + 3 * i2 - i3;
+		float b1 = 3 * i/100 - 6 * i2 + 3 * i3;
+		float b2 = 3 * i2 - 3 * i3;
+		float b3 = i3;
+
 		Vector2 vec2;
-		vec2.x = xAimPoint;
-		vec2.y = yAimPoint;
+		vec2.x = b0 * 700 + b1 * 500 + b2 * 400 + b3 * 100;
+		vec2.y = b0 * 500 + b1 * 100 + b2 * 100 + b3 * 500;
+
+
 		MoveArray.push_back(vec2);
-		xAimPoint-=4;
-		yAimPoint-=2;
+
 	}
 }
 
